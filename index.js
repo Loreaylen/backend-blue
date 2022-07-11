7623f95168e138410fdbb35944229150cef6ad71
Lorena Aylen <lag9512@gmail.com>
2022-07-11 17:13:29 -0300

    Archivo JSON agregado. Se añadieron endpoints

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const dolarPrueba = require("./notasDePrueba.json")
+
+console.log(dolarPrueba.valoresDolar.find(dolar => dolar.id === 0))
+
 const express = require('express')
 const app = express()
 
-// const app = http.createServer((request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'text/plain'})
-//     response.end("Que onda perro")
-// })
 
 app.get('/', (request,response) =>{
-    response.send('<h1>Que onda prro</h1>')
+    response.send('<h1>Esto es localhost</h1>')
+})
+
+app.get('/api/dolar', (request, response) => {
+    response.json(dolarPrueba.valoresDolar)
+})
+
+app.get('/api/dolar/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const dolar = dolarPrueba.valoresDolar.find(dolar => dolar.id === id)
+    
+    response.json(dolar)
 })
 
 const PORT = 3001
