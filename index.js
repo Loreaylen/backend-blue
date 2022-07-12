bf4183c8f52fcfd6c03268cf6ca8062090dc1029
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2022-07-11 21:03:31 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-
+const cors = require("cors");
 const endpoints = require('./routers/endpoints')
+const PORT = process.env.PORT || 3001;
 
 app.use('/', endpoints)
+app.use(cors({
+  origin: '*'
+}));
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT)
-console.log(`Servidor corriendo en puerto ${PORT}`)
+app.listen(PORT);
+console.log(`Servidor corriendo en puerto ${PORT}`);
