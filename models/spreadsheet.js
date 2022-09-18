1104c3b56680b15af6b9877c3d71824226686993
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2022-09-18 04:27:31 -0300

    update files

--- models/spreadsheet.js
+++ models/spreadsheet.js
@@ -1,15 +1,11 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet'),
       doc = new GoogleSpreadsheet("1ZAlbj1a3I4D-qePNzdbzgDSx0o4KPWs3khtJspvK6b0");
 
-async function accesoGoogleSheet() {
-
+module.exports = async function accesoGoogleSheet() {
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_ACCOUNT,
     private_key: process.env.PRIVATE_KEY,
   });
   await doc.loadInfo()
   return doc
-
-}
-
-module.exports = accesoGoogleSheet;
+};
--- routers/endpoints.js
+++ routers/endpoints.js
@@ -1,15 +1,17 @@
 const {Router} = require('express'),
       router = Router(),
       llamadaExcel = require('../models/spreadsheet'),
-      dolarPrueba = require("../notasDePrueba.json"),
-      fecha = new Date();
+      dolarPrueba = require("../notasDePrueba.json");
 
 router.get('/', (request,response) =>{
   response.send('<h1>Esto es localhost</h1>')
 })
 
 router.get('/api/dolar', (request, response) => {
-  const diaYHora = fecha.getDate() + "/" + (fecha.getMonth() + 1) + " " + fecha.getHours() + ":" + fecha.getMinutes();
+  const fecha = new Date(),
+        minutes = fecha.getMinutes(),
+        formatDate = fecha.getDate() + '/' + (fecha.getMonth() + 1) + ' ' + fecha.getHours() + ':' + (minutes < 10 ? `0${minutes}` : minutes);
+
   llamadaExcel()
   .then(document => document.sheetsByIndex[0])
   .then(sheet => sheet.getRows())
@@ -23,11 +25,12 @@ router.get('/api/dolar', (request, response) => {
         t: element.tipo,
         c: element.compra,
         v: element.venta,
-        d: diaYHora
+        d: formatDate
       })
-    }
-    response.send(values)
+    };
+    return response.json(values);
   })
+  .catch(err => response.send(err))
 
 })
 
